ca92a9a2b9d1674f7ac27888160c08660ee2da5e
esponges <esponges@gmail.com>
2023-03-08 16:38:50 -0600

    first commit

---
+++ src/main/java/com/example/mySrpingProject/MySrpingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mySrpingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySrpingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySrpingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mySrpingProject/MySrpingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mySrpingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySrpingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
